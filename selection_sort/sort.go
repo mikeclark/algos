701e394f3599174c58d3ac6175e940fd93cd05c5
Mike Clark <mike@mike-clark.org>
2021-10-28 22:07:05 -0400

    added test for selection sort

---
+++ selection_sort/sort.go
@@ -0,0 +1,5 @@
+package selection_sort
+
+func Sort(arr []int) []int {
+	return arr
+}
---
+++ selection_sort/sort_test.go
@@ -0,0 +1,20 @@
+package selection_sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	assertSortedArray := func(t testing.TB, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("Testing Sort 1", func(t *testing.T) {
+		testArray := []int{1, 45, 6, 67, 7, 82, 89, 2}
+		got := Sort(testArray)
+		want := []int{1, 2, 6, 7, 45, 67, 82, 89}
+		assertSortedArray(t, got, want)
+	})
+}
