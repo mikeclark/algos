9beb51200a8ba7cbc7b0bd22f2e0db6b6bf792ae
Mike Clark <mike@mike-clark.org>
2021-10-28 22:21:04 -0400

    added selection sort

--- selection_sort/sort.go
+++ selection_sort/sort.go
@@ -1,5 +1,15 @@
 package selection_sort
 
 func Sort(arr []int) []int {
+	num := len(arr)
+	for x := 0; x < num; x++ {
+		first := x
+		for next := x; next < num; next++ {
+			if arr[next] < arr[first] {
+				first = next
+			}
+		}
+		arr[x], arr[first] = arr[first], arr[x]
+	}
 	return arr
 }
